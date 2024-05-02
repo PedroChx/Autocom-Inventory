d9d6f9fd269d64f328ebe36cead1a093cc79c0e7
Pedro Chávez <pchavez28@alumnos.uaq.mx>
2024-05-02 12:34:22 -0600

    Project Structure

--- db/config.ts
+++ db/config.ts
@@ -7,7 +7,6 @@ const User = defineTable({
     id: column.text({ primaryKey: true, optional: false, unique: true }),
     username: column.text({ unique: true, optional: false }),
     password: column.text({ optional: true }),
-    github_id: column.text({ optional: true, unique: true }),
   },
 });
 
